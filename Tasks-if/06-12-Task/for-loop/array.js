b993a2871014f36650fb12dbc406d13ed7b57a9b
Nihala-ms <nihalabintsalih@gmail.com>
2025-12-08 11:03:38 +0530

    added more task

--- Tasks-if/06-12-Task/for-loop/array.js
+++ Tasks-if/06-12-Task/for-loop/array.js
@@ -1,1 +1,6 @@
-vfcgcyy i==
+var arr=[12,34,56,34,65,67];
+for(var i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+    
+}
---
+++ Tasks-if/06-12-Task/for-loop/largest.js
@@ -0,0 +1,10 @@
+var arr=[23,78,45,76,56,67];
+var max=arr[0];
+for(var i=0;i<arr.length;i++)
+{
+    if(arr[i]>max)
+        {
+            max=arr[i]
+        }    
+}
+console.log(max);
